32fcb77933b310b749c32e22b81eec61d60f5829
Nicolas Earnshaw <nearnshaw@decentraland.org>
2021-05-21 18:36:58 -0300

    single player

--- src/fireworks.ts
+++ src/fireworks.ts
@@ -1,18 +1,14 @@
 import * as utils from '@dcl/ecs-scene-utils'
-import { sceneMessageBus } from './messageBus'
 
 export class Firework extends Entity {
   private fireworkLaunchSound = new AudioClip('sounds/fireworkLaunch.mp3')
   private fireworkExplodeSound = new AudioClip('sounds/fireworkExplode.mp3')
 
-  id: number 
-
-  constructor(model: GLTFShape, transform: Transform, id:number) {
+  constructor(model: GLTFShape, transform: Transform) {
     super()
     engine.addEntity(this)
     this.addComponent(model)
     this.addComponent(transform)
-    this.id = id
 
     this.addComponent(new Animator())
     this.getComponent(Animator).addClip(
@@ -22,8 +18,21 @@ export class Firework extends Entity {
     this.addComponentOrReplace(
       new OnPointerDown(
         () => {
-          sceneMessageBus.emit("launch", {id: this.id})
-         
+
+          // sound
+          this.addComponentOrReplace(new AudioSource(this.fireworkLaunchSound))
+          this.getComponent(AudioSource).playOnce()
+          utils.setTimeout(1250, () => {
+            this.addComponentOrReplace(new AudioSource(this.fireworkExplodeSound))
+            this.getComponent(AudioSource).playOnce()
+          })
+
+          // animation
+          this.getComponent(Animator).getClip('Play').play()
+          utils.setTimeout(4800, () => {
+            this.getComponent(Animator).getClip('Play').stop()
+          })
+
         },
         {
           hoverText: 'Launch firework',
@@ -32,22 +41,6 @@ export class Firework extends Entity {
         }
       )
     )
-  }
-  launch(){
-    // sound
-    this.addComponentOrReplace(new AudioSource(this.fireworkLaunchSound))
-    this.getComponent(AudioSource).playOnce()
-    utils.setTimeout(1250, () => {
-      this.addComponentOrReplace(new AudioSource(this.fireworkExplodeSound))
-      this.getComponent(AudioSource).playOnce()
-    })
-
-    // animation
-    this.getComponent(Animator).getClip('Play').play()
-    utils.setTimeout(4800, () => {
-      this.getComponent(Animator).getClip('Play').stop()
-    })
-    
   }
 }
 
@@ -61,8 +54,6 @@ hayBase.addComponent(
 )
 engine.addEntity(hayBase)
 
-let fireworks : Firework[] = []
-
 const fireworkShape = new GLTFShape('models/Firework_03.gltf')
 
 export const firework1 = new Firework(
@@ -71,7 +62,6 @@ export const firework1 = new Firework(
     position: new Vector3(-0.1, 0.1, -0.8),
     rotation: Quaternion.Euler(-45, 45, 25),
   }),
-  0
 )
 firework1.setParent(hayBase)
 
@@ -81,7 +71,6 @@ export const firework2 = new Firework(
     position: new Vector3(0.1, 0.1, 0.8),
     rotation: Quaternion.Euler(-45, 45 + 180, -25),
   }),
-  1
 )
 firework2.setParent(hayBase)
 
@@ -91,7 +80,6 @@ export const firework3 = new Firework(
     position: new Vector3(0, 0.125, 0),
     rotation: Quaternion.Euler(2, 90, -2),
   }),
-  2
 )
 firework3.setParent(hayBase)
 
@@ -101,21 +89,5 @@ export const firework4 = new Firework(
     position: new Vector3(-0.1, 0.1, 0.8),
     rotation: Quaternion.Euler(45, -45, 25),
   }),
-  3
 )
 firework4.setParent(hayBase)
-
-
-fireworks.push(firework1)
-fireworks.push(firework2)
-fireworks.push(firework3)
-fireworks.push(firework4)
-
-
-sceneMessageBus.on("launch", (data)=>{
-  log("LAUNCHING", data.id)
-  fireworks[data.id].launch()
-})
-
-
-
